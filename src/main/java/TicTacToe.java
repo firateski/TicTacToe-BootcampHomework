38959b041cc72f57d91435af7c3d02bc87c669db
Fırat Eşki <frat888@gmail.com>
2020-09-25 01:57:35 +0300

    Implement getCellValue and setCellValue methods

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,2 +1,15 @@
 public class TicTacToe {
+    private final CellValue[][] table =
+            {
+                { CellValue.EMPTY, CellValue.EMPTY, CellValue.EMPTY },
+                { CellValue.EMPTY, CellValue.EMPTY, CellValue.EMPTY },
+                { CellValue.EMPTY, CellValue.EMPTY, CellValue.EMPTY }
+            };
+    public CellValue getCellValue(int x, int y){
+        return table[x][y];
+    }
+
+    public void setCellValue(CellValue state, int x, int y){
+        table[x][y] = state;
+    }
 }
