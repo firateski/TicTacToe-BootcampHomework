44172b0eac225705dfaa6c8d8e0a0961bf0d959d
Fırat Eşki <frat888@gmail.com>
2020-09-25 01:58:15 +0300

    Create tests for isTableFilled method

--- src/test/java/TicTacToeTests.java
+++ src/test/java/TicTacToeTests.java
@@ -29,4 +29,52 @@ public class TicTacToeTests {
         // Verify
         assertEquals(CellValue.O, output);
     }
+
+    @Test
+    public void isTableFilled_shouldReturnTrue_whenTableIsFilled(){
+        // Arrange
+        TicTacToe sut = new TicTacToe();
+
+        // Fill table using nested loops
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                CellValue currentCellValue = j % 2 == 0 ? CellValue.X : CellValue.O;
+                sut.setCellValue(currentCellValue, i, j);
+            }
+        }
+
+        // Act
+        boolean output = sut.isTableFilled();
+
+        // Verify
+        assertTrue(output);
+    }
+
+    @Test
+    public void isTableFilled_shouldReturnFalse_whenTableIsEmpty(){
+        // Arrange
+        TicTacToe sut = new TicTacToe();
+
+        // Act
+        boolean output = sut.isTableFilled();
+
+        // Verify
+        assertFalse(output);
+    }
+
+    @Test
+    public void isTableFilled_shouldReturnFalse_whenTableIsPartiallyFilled(){
+        // Arrange
+        TicTacToe sut = new TicTacToe();
+        sut.setCellValue(CellValue.X, 0, 0);
+        sut.setCellValue(CellValue.O, 0, 1);
+        sut.setCellValue(CellValue.X, 1, 0);
+        sut.setCellValue(CellValue.O, 2, 0);
+
+        // Act
+        boolean output = sut.isTableFilled();
+
+        // Verify
+        assertFalse(output);
+    }
 }
