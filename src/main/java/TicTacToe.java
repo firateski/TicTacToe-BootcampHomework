e9386db1f3e3e6b0c287121f3481ba2bb2b98ba1
Fırat Eşki <frat888@gmail.com>
2020-09-25 01:58:42 +0300

    Implement isTableFilled method

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -5,11 +5,22 @@ public class TicTacToe {
                 { CellValue.EMPTY, CellValue.EMPTY, CellValue.EMPTY },
                 { CellValue.EMPTY, CellValue.EMPTY, CellValue.EMPTY }
             };
+    
     public CellValue getCellValue(int x, int y){
         return table[x][y];
     }
 
     public void setCellValue(CellValue state, int x, int y){
         table[x][y] = state;
+    }
+
+    public boolean isTableFilled() {
+        for(int i = 0; i < table.length; i++){
+            for(int j = 0; j < table[i].length; j++){
+                if(getCellValue(i, j) == CellValue.EMPTY) return false;
+            }
+        }
+
+        return true;
     }
 }
