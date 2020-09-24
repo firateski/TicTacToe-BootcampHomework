4fdf7e70426589532fcfbde45eca2b86072d6f0a
Fırat Eşki <frat888@gmail.com>
2020-09-25 01:57:10 +0300

    Create tests for getCellValue and setCellValue methods

--- src/test/java/TicTacToeTests.java
+++ src/test/java/TicTacToeTests.java
@@ -1,2 +1,32 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TicTacToeTests {
+
+    @Test
+    public void setCellValue_shouldSetCorrectPlace(){
+        // Arrange
+        TicTacToe sut = new TicTacToe();
+        sut.setCellValue(CellValue.X, 1, 1);
+
+        // Act
+        CellValue output = sut.getCellValue(1, 1);
+
+        // Verify
+        assertEquals(CellValue.X, output);
+    }
+
+    @Test
+    public void getCellValue_shouldGetCorrectValue(){
+        // Arrange
+        TicTacToe sut = new TicTacToe();
+        sut.setCellValue(CellValue.O, 0, 2);
+
+        // Act
+        CellValue output = sut.getCellValue(0, 2);
+
+        // Verify
+        assertEquals(CellValue.O, output);
+    }
 }
